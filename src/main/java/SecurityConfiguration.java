1fcc36f4711b81c06dc8fbacb359129af40104ae
osemes1 <130790511+osemes1@users.noreply.github.com>
2023-09-26 12:14:50 +0300

    Commit DZ35 2023-0926 12:15

---
+++ src/main/java/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/ping").permitAll() // Доступ без авторизації до /ping
+                .antMatchers(HttpMethod.POST, "/products/**").hasRole("ADMIN") // Доступ тільки для ADMIN
+                .antMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN") // Доступ тільки для ADMIN
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic(); // Включаємо Basic Authentication
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("user").password(encoder.encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(encoder.encode("admin")).roles("ADMIN");
+    }
+}

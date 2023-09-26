0ab0b2607e207d931dab965510f6adc1eb96920d
osemes1 <130790511+osemes1@users.noreply.github.com>
2023-09-26 12:14:03 +0300

    Commit DZ35 2023-0926 12:15

---
+++ src/main/java/Product.java
@@ -0,0 +1,50 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "o_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double cost;
+
+    public Product() {
+    }
+
+    public Product(String name, Double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+}
